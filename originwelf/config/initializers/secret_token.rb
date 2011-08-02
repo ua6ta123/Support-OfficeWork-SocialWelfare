4604046a8fc57a7e15190d91c66737d4946105c0
Fumiaki Sakamoto <ua6ta123@bma.biglobe.ne.jp>
2011-08-02 16:28:22 +0900

    first implement

---
+++ originwelf/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Originwelf::Application.config.secret_token = '3d5b075aef9d2c3f702a07e2e48d9b846cc904490c5ae9bc49713aa6126ea826bcb3c5d3fad7623581f2136894e614abc8b09169a388ca7819c6022428110eff'
